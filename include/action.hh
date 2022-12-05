9de5e4ad2bc98c847d44874b4703c83c42efc4fa
francesco pascolini <pasco@francesco-pc>
2022-12-05 23:55:55 +0100

    merging of action and generator files for practicity

--- include/action.hh
+++ include/action.hh
@@ -2,13 +2,29 @@
 #define ACTION_HH
 
 #include "G4VUserActionInitialization.hh"
+#include "G4VUserPrimaryGeneratorAction.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4ParticleGun.hh"
+#include "G4ParticleTable.hh"
+
 #include "generator.hh"
-class ActionInitialization : public G4VUserActionInitialization{
+
+class MyActionInitialization : public G4VUserActionInitialization{
 public:
-  ActionInitialization();
-  ~ActionInitialization();
+  MyActionInitialization();
+  ~MyActionInitialization();
   
   virtual void Build() const; 
+};
+
+class MyPrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction{
+public:
+  MyPrimaryGeneratorAction();
+  ~MyPrimaryGeneratorAction();
+
+  virtual void GeneratePrimaries(G4Event* anEvent);
+private:
+  G4ParticleGun *fParticleGun;
 };
 
 #endif
--- src/action.cc
+++ src/action.cc
@@ -1,9 +1,32 @@
 #include "action.hh"
 
-ActionInitialization::ActionInitialization(){}
-ActionInitialization::~ActionInitialization(){}
+MyActionInitialization::MyActionInitialization(){}
+MyActionInitialization::~MyActionInitialization(){}
 
-void ActionInitialization::Build() const{
+void MyActionInitialization::Build() const{
   PrimaryGenerator *generator = new PrimaryGenerator();
   SetUserAction(generator);
+}
+
+MyPrimaryGeneratorAction::MyPrimaryGeneratorAction(){
+  fParticleGun = new G4ParticleGun(1);//creating just 1 primary vertex per particle
+}
+MyPrimaryGeneratorAction::~MyPrimaryGeneratorAction(){
+  delete fParticleGun;
+}
+
+void MyPrimaryGeneratorAction::GeneratePrimaries(G4Event *anEvent){
+  G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
+  G4String particleName = "proton";
+  G4ParticleDefinition *particle = particleTable->FindParticle("proton");
+
+  G4ThreeVector pos(0.,0.,0.);//particle generated at the origin
+  G4ThreeVector mom(0.,0.,1.);//particle generated along z-direction
+
+  fParticleGun->SetParticlePosition(pos);
+  fParticleGun->SetParticleMomentumDirection(mom);
+  fParticleGun->SetParticleMomentum(4.*MeV);
+  fParticleGun->SetParticleDefinition(particle);
+
+  fParticleGun->GeneratePrimaryVertex(anEvent);
 }
