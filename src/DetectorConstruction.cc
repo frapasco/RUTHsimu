86524249a9b863f22390b9a0e466efdcc79db5ee
francesco pascolini <pasco@francesco-pc>
2022-12-05 21:25:32 +0100

    fixed positionings and geometries

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -7,6 +7,7 @@ G4VPhysicalVolume* DetectorConstruction::Construct(){
   //--------------------------------------------------------
   //visualization attributes--------------------------------
   //--------------------------------------------------------
+
   G4VisAttributes* solidGreen = new G4VisAttributes(G4Colour(0.3,0.8,0.1));
   solidGreen->SetVisibility(true);
   solidGreen->SetForceSolid(TRUE);
@@ -27,21 +28,22 @@ G4VPhysicalVolume* DetectorConstruction::Construct(){
   //--------------------------------------------------------
   //materials-----------------------------------------------
   //--------------------------------------------------------
+
   G4NistManager* nist = G4NistManager::Instance();
   
-  G4Material* defaultMat = nist->FindOrBuildMaterial("G4_AIR");
+  G4Material* defaultMat = nist->FindOrBuildMaterial("G4_AIR");//air
   G4Material* Al = nist->FindOrBuildMaterial("G4_Al");
   G4Material* Au = nist->FindOrBuildMaterial("G4_Au");
-  G4Material* Am= nist->FindOrBuildMaterial("G4_Am");//frapasco->to be modified with the correct isotope, for simplicity let's approx Am+Au layer to just Am since we don't have the proportions of the alloy
+  G4Material* Am= nist->FindOrBuildMaterial("G4_Am");
   G4Material* Ag = nist->FindOrBuildMaterial("G4_Ag");
   G4Isotope* Am241 = new G4Isotope("Am241", 95, 241, 241.056829*g/mole); // atomic number, number of nucleons, mass of mole
   G4Element* elAm241 = new G4Element("elAm241", "Am-241", 1); //name, symbol, number of isotopes
   elAm241->AddIsotope(Am241, 100*perCent); //name, abundance
-  G4Material* Americio241 = new G4Material("Americio241", 12 *g/cm3,1);
-  Americio241->AddElement(elAm241,1);
+  G4Material* Americium241 = new G4Material("Americium241", 12 *g/cm3,1);
+  Americium241->AddElement(elAm241,1);
   G4Material* AuTarget = new G4Material("AuTarget", 79, 196.96657*g/mole, 325.e-6 *g/cm3);
 
-  //vacuum da modificare
+  //vacuum to be implemented
   G4double atomicNumber = 1.;
   G4double massOfMole = 1.008*g/mole;
   G4double density = 1.e-25*g/cm3;
@@ -49,6 +51,45 @@ G4VPhysicalVolume* DetectorConstruction::Construct(){
   G4double pressure = 3.e-18*pascal;
   G4Material* vacuum = new G4Material("interGalactic", atomicNumber, massOfMole, density, kStateGas, temperature, pressure);
 
+  
+  //--------------------------------------------------------
+  //parameters----------------------------------------------
+  //--------------------------------------------------------
+  
+  //parameters that describe the source geometry
+  G4double sourceExtDiameter = 12.7*mm;
+  G4double coreDiameter = 7.*mm;
+  G4double sourceTotLength = 79.*mm;
+  G4double AuLayer1Thickness = 0.00051*mm;//layer A
+  G4double AuLayer2Thickness = 0.0010*mm;//layer B
+  G4double AmLayerThickness = 0.00051*mm;//layer C
+  G4double AuLayer3Thickness = 0.0008*mm;//layer D
+  G4double AgLayerThickness = 0.229*mm;//layer F
+  G4double sourceThickness = AuLayer1Thickness+AuLayer2Thickness+AmLayerThickness+AuLayer3Thickness+AgLayerThickness;
+
+  //parameters of collimators and target geometry
+  //C1
+  G4double suppColl1X=12.05*mm;
+  G4double suppColl1Y=25.05*mm;
+  G4double collH1X = 4.05*mm;
+  G4double collH1Y = 7.05*mm;
+  G4double coll1Thickness = 1.55*mm;
+  G4double distC1 = 16.75*mm;
+  //C2
+  G4double suppColl2X=12.05*mm;
+  G4double suppColl2Y=25.00*mm;
+  G4double collH2X = 4.10*mm;
+  G4double collH2Y = 7.05*mm;
+  G4double coll2Thickness = 1.60*mm;
+  G4double distC2 = 0.*mm;//it is 0 since C2 and the source are in contact
+  //target
+  G4double targetDiameter = 25. *mm;
+  G4double targetThickness = 0.5 *mm;
+  G4double distTarget = 1.10*mm;
+
+  //totals
+  G4double totLength = sourceTotLength+distC2+coll2Thickness+distC1+coll1Thickness+distTarget+targetThickness;
+  
   //--------------------------------------------------------
   //world volume--------------------------------------------
   //--------------------------------------------------------
@@ -60,33 +101,19 @@ G4VPhysicalVolume* DetectorConstruction::Construct(){
   //source support------------------------------------------
   //--------------------------------------------------------
   //mother volume
-  G4Box* sourceSuportMother = new G4Box("supportMother", 0.5*(32.45*mm), 0.5*(104.05*mm), 0.5*(70.05*mm+31.25*mm));
-
+  G4Box* support = new G4Box("support", 12.05*mm, 50.*mm, 0.5*totLength);
+  G4LogicalVolume* supportLog = new G4LogicalVolume(support, defaultMat, "supportLog");
+  new G4PVPlacement(0, G4ThreeVector(0.*mm, 0.*mm, -(totLength-0.5*sourceTotLength)), supportLog, "support", worldLog, false, 0);
+  
   //--------------------------------------------------------
   //source--------------------------------------------------
   //--------------------------------------------------------
-  //parameters that describe the source geometry
-  G4double sourceExtDiameter = 12.7*mm;
-  G4double coreDiameter = 7.*mm;
-  G4double sourceTotLength = 79.*mm;
-  G4double AuLayer1Thickness = 0.00051*mm;//layer A
-  G4double AuLayer2Thickness = 0.0010*mm;//layer B
-  G4double AmLayerThickness = 0.00051*mm;//layer C
-  G4double AuLayer3Thickness = 0.0008*mm;//layer D
-  G4double AgLayerThickness = 0.229*mm;//layer F
-  G4double sourceThickness = AuLayer1Thickness+AuLayer2Thickness+AmLayerThickness+AuLayer3Thickness+AgLayerThickness;
-  //parameters of collimators and target geometry
-  G4double coll1X = 4 *mm;
-  G4double coll1Y = 7 *mm;
-  G4double coll2X = 4 *mm;
-  G4double coll2Y = 7 *mm;
-  G4double targetDiameter = 25. *mm;
-  G4double targetThickness = 0.5 *mm;
 
+  
   //mother volume
   G4Tubs* sourceMother = new G4Tubs("sourceMother", 0*mm, 0.5*sourceExtDiameter, 0.5*sourceTotLength, 0.*deg, 360.*deg);
   G4LogicalVolume* sourceLog = new G4LogicalVolume(sourceMother, defaultMat, "sourceMotherLog");
-  new G4PVPlacement(0, G4ThreeVector(0.*mm, 0.*mm, 0.*mm), sourceLog, "source", worldLog, false, 0);//frapasco->positioning of the full source
+  new G4PVPlacement(0, G4ThreeVector(0.*mm, 0.*mm, 0.*mm), sourceLog, "source", supportLog, false, 0);
   
   //case
   G4Tubs* caseCyl = new G4Tubs("caseCyl", 0.*mm, 0.5*sourceExtDiameter, 0.5*(sourceTotLength-sourceThickness-1.*mm), 0.*deg, 360.*deg);
@@ -102,10 +129,10 @@ G4VPhysicalVolume* DetectorConstruction::Construct(){
 
   //source core
   G4Tubs* sourceCore = new G4Tubs("sourceCore", 0.*mm, 0.5*coreDiameter, 0.5*sourceThickness, 0.*deg, 360.*deg);
-  G4LogicalVolume* sourceCoreLog = new G4LogicalVolume(sourceCore, defaultMat, "AgLayerLog");
+  G4LogicalVolume* sourceCoreLog = new G4LogicalVolume(sourceCore, defaultMat, "sourceCore");
   sourceCoreLog->SetVisAttributes(solidGreen);
   new G4PVPlacement(0, G4ThreeVector(0., 0.,
-				     0.5*(sourceTotLength-sourceThickness)-1.*mm),/*added at the end of the cylinder and inside the tube*/
+				     0.5*(sourceTotLength-sourceThickness)-1.*mm),//added at the end of the cylinder and inside the tube
 		    sourceCoreLog, "sourceCore", sourceLog, false, 0);
   //for positioning the core elements one has to consider that each layer is after the full lenght of the previous one,
   //   and since the G4Tubs is positioned in the middle of his length one has to multiply by 0.5 the thickness
@@ -116,60 +143,61 @@ G4VPhysicalVolume* DetectorConstruction::Construct(){
   G4LogicalVolume* AuLayer1Log = new G4LogicalVolume(AuLayer1, Au, "AuLayer1Log");
   AuLayer1Log->SetVisAttributes(solidYellow);
   new G4PVPlacement(0, G4ThreeVector(0.*m, 0.*m, 0.5*sourceThickness-0.5*AuLayer1Thickness), 
-              AuLayer1Log, "AuLayer1", sourceCoreLog, false, 0);
+		    AuLayer1Log, "AuLayer1", sourceCoreLog, false, 0);
 
   //Au2
   G4Tubs* AuLayer2 = new G4Tubs("AuLayer2", 0.*mm, 0.5*coreDiameter, 0.5*AuLayer2Thickness, 0.*deg, 360.*deg);
   G4LogicalVolume* AuLayer2Log = new G4LogicalVolume(AuLayer2, Au, "AuLayer2Log");
   AuLayer2Log->SetVisAttributes(solidYellow);
   new G4PVPlacement(0, G4ThreeVector(0.*m, 0.*m, 0.5*sourceThickness-AuLayer1Thickness-0.5*AuLayer2Thickness), 
-              AuLayer2Log, "AuLayer2", sourceCoreLog, false, 0);
+		    AuLayer2Log, "AuLayer2", sourceCoreLog, false, 0);
 
   //Am
   G4Tubs* AmLayer = new G4Tubs("AmLayer", 0.*mm, 0.5*coreDiameter, 0.5*AmLayerThickness, 0.*deg, 360.*deg);
-  G4LogicalVolume* AmLayerLog = new G4LogicalVolume(AmLayer, Americio241, "AmLayerLog");
+  G4LogicalVolume* AmLayerLog = new G4LogicalVolume(AmLayer, Americium241, "AmLayerLog");
   AmLayerLog->SetVisAttributes(solidSilver);
   new G4PVPlacement(0, G4ThreeVector(0.*m, 0.*m, 0.5*sourceThickness-AuLayer1Thickness-AuLayer2Thickness-0.5*AmLayerThickness),
-		          AmLayerLog, "AmLayer", sourceCoreLog, false, 0);
+		    AmLayerLog, "AmLayer", sourceCoreLog, false, 0);
 
   //Au3
   G4Tubs* AuLayer3 = new G4Tubs("AuLayer3", 0.*mm, 0.5*coreDiameter, 0.5*AuLayer3Thickness, 0.*deg, 360.*deg);
   G4LogicalVolume* AuLayer3Log = new G4LogicalVolume(AuLayer3, Au, "AuLayer3Log");
   AuLayer3Log->SetVisAttributes(solidYellow);
   new G4PVPlacement(0, G4ThreeVector(0.*m, 0.*m, 0.5*sourceThickness-AuLayer1Thickness-AuLayer2Thickness-AmLayerThickness-0.5*AuLayer3Thickness),
-		          AuLayer3Log, "AuLayer3", sourceCoreLog, false, 0);
+		    AuLayer3Log, "AuLayer3", sourceCoreLog, false, 0);
 
   //Ag
   G4Tubs* AgLayer = new G4Tubs("AgLayer", 0.*mm, 0.5*coreDiameter, 0.5*AgLayerThickness, 0.*deg, 360.*deg);
   G4LogicalVolume* AgLayerLog = new G4LogicalVolume(AgLayer, Ag, "AgLayerLog");
   AgLayerLog->SetVisAttributes(solidSilver);
-  new G4PVPlacement(0, G4ThreeVector(0.*m, 0.*m, 1*sourceThickness-AuLayer1Thickness-AuLayer2Thickness-AmLayerThickness-AuLayer3Thickness-0.5*AgLayerThickness),
-		          AgLayerLog, "AgLayer", sourceCoreLog, false, 0);
+  new G4PVPlacement(0, G4ThreeVector(0.*m, 0.*m, 0.5*sourceThickness-AuLayer1Thickness-AuLayer2Thickness-AmLayerThickness-AuLayer3Thickness-0.5*AgLayerThickness),
+		    AgLayerLog, "AgLayer", sourceCoreLog, false, 0);
 
-  //Collimator1
-  G4VSolid* suppColl1 = new G4Box("SupportCollimator1", 10*mm, 10*mm, 1*mm);
-  G4VSolid* holeColl1 = new G4Box("HoleCollimator2", 0.5*coll1X, 0.5*coll1Y, 1*mm);
-  G4VSolid* Collimator1 = new G4SubtractionSolid("SupportCollimator1-HoleCollimator1", suppColl1, holeColl1, 0, G4ThreeVector(0.,0.,0.)); //l4rnone->non sono sicura del threevector
+  //Collimator 1
+  G4VSolid* suppColl1 = new G4Box("SupportCollimator1", 0.5*suppColl1X, 0.5*suppColl1Y, 0.5*coll1Thickness);
+  G4VSolid* holeColl1 = new G4Box("HoleCollimator2", 0.5*collH1X, 0.5*collH1Y, 0.5*coll1Thickness);
+  G4VSolid* Collimator1 = new G4SubtractionSolid("SupportCollimator1-HoleCollimator1", suppColl1, holeColl1, 0, G4ThreeVector(0.*mm,-0.5*suppColl1Y+0.5*collH1Y+2.60*mm,0.*mm));
   G4LogicalVolume* Coll1Log = new G4LogicalVolume(Collimator1, Al, "Coll1Log");
-  Coll1Log->SetVisAttributes(solidSilver); //non capisco perchè colora anche il buco
-  new G4PVPlacement(0, G4ThreeVector(0.*m, 0.*m, 1.1*sourceThickness-AuLayer1Thickness-AuLayer2Thickness-AmLayerThickness-AuLayer3Thickness-0.5*AgLayerThickness+45*mm),
-		          Coll1Log, "Collimator1", sourceCoreLog, false, 0); //da cambiare G4ThreeVector
+  Coll1Log->SetVisAttributes(solidSilver);
+  new G4PVPlacement(0, G4ThreeVector(0, 0.5*sourceExtDiameter, 0.5*sourceTotLength+distC2+coll2Thickness+distC1+0.5*coll1Thickness),
+		    Coll1Log, "Collimator1", supportLog, false, 0); //da cambiare G4ThreeVector
   
-  //Collimator2
-  G4VSolid* suppColl2 = new G4Box("SupportCollimator2", 10*mm, 10*mm, 1*mm);
-  G4VSolid* holeColl2 = new G4Box("HoleCollimator2", 0.5*coll2X, 0.5*coll2Y, 1*mm);
-  G4VSolid* Collimator2 = new G4SubtractionSolid("SupportCollimator2-HoleCollimator2", suppColl2, holeColl2, 0, G4ThreeVector(0.,0.,0.)); 
+  //Collimator 2
+  G4VSolid* suppColl2 = new G4Box("SupportCollimator2", 0.5*suppColl2X, 0.5*suppColl2Y, 0.5*coll2Thickness);
+  G4VSolid* holeColl2 = new G4Box("HoleCollimator2", 0.5*collH2X, 0.5*collH2Y, 0.5*coll2Thickness);
+  G4VSolid* Collimator2 = new G4SubtractionSolid("SupportCollimator2-HoleCollimator2", suppColl2, holeColl2, 0, G4ThreeVector(0.*mm,-0.5*suppColl2Y+0.5*collH2Y+2.55*mm,0.*mm)); 
   G4LogicalVolume* Coll2Log = new G4LogicalVolume(Collimator2, Al, "Coll2Log");
   Coll2Log->SetVisAttributes(solidSilver);
-  new G4PVPlacement(0, G4ThreeVector(0.*m, 0.*m, 0.5*sourceThickness-AuLayer1Thickness-AuLayer2Thickness-AmLayerThickness-AuLayer3Thickness-0.5*AgLayerThickness+16*mm),
-		          Coll2Log, "Collimator2", sourceCoreLog, false, 0); //da cambiare G4ThreeVector
+  new G4PVPlacement(0, G4ThreeVector(0,0.5*sourceExtDiameter,0.5*sourceTotLength+distC2+0.5*coll2Thickness),
+		    Coll2Log, "Collimator2", supportLog, false, 0); //da cambiare G4ThreeVector
   
   //Target
   G4Tubs* Target = new G4Tubs("Target", 0.*mm, 0.5*targetDiameter, 0.5*targetThickness, 0.*deg, 360.*deg);
   G4LogicalVolume* TargetLog = new G4LogicalVolume(Target, AuTarget, "TargetLog");
   TargetLog->SetVisAttributes(solidYellow);
-  new G4PVPlacement(0, G4ThreeVector(0.*m, 0.*m, 0.5*sourceThickness-AuLayer1Thickness-AuLayer2Thickness-AmLayerThickness-AuLayer3Thickness-0.5*AgLayerThickness+77*mm),
-		          TargetLog, "Target", sourceCoreLog, false, 0); //da cambiare G4ThreeVector
+  new G4PVPlacement(0, G4ThreeVector(0,0,0.5*sourceTotLength+distC2+coll2Thickness+distC1+coll1Thickness+distTarget+0.5*targetThickness),
+		    TargetLog, "Target", supportLog, false, 0); //da cambiare G4ThreeVector
 
+  
   return PhysicalWorld;
 }
