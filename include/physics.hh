f2ba75f4f8fc238a3fb723d70e640cdb2b76d223
l4rnone <lisa@retemia.net>
2022-12-12 11:15:16 +0100

    alpha decay

--- include/physics.hh
+++ include/physics.hh
@@ -4,8 +4,9 @@
 #include "G4VModularPhysicsList.hh"
 #include "G4EmStandardPhysics.hh"
 #include "G4OpticalPhysics.hh"
-#include "G4RadioactiveDecayPhysics.hh"
 #include "G4DecayPhysics.hh"
+#include "G4RadioactiveDecayPhysics.hh"
+
 
 class PhysicsList : public G4VModularPhysicsList
 {
--- src/action.cc
+++ src/action.cc
@@ -8,34 +8,40 @@ void ActionInitialization::Build() const{
   SetUserAction(generator);
 }
 
-PrimaryGenerator::PrimaryGenerator(){
+PrimaryGenerator::PrimaryGenerator()
+{
   fParticleGun = new G4ParticleGun(1);//creating just 1 primary vertex per particle
 
   G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
-  G4String particleName = "proton";
+  //G4String particleName = "proton";
   G4ParticleDefinition *particle = particleTable->FindParticle("geantino");
   
-  G4ThreeVector pos(0.,0.,0.);//particle generated at the origin
+  G4ThreeVector pos(0.,0.,0.- 1*mm);//particle generated at the origin
+  //G4ThreeVector pos(0.,0.,0.5*sourceThickness-AuLayer1Thickness-AuLayer2Thickness-0.5*AmLayerThickness); /generated at center of Am241
   G4ThreeVector mom(0.,0.,1.);//particle generated along z-direction
 
   fParticleGun->SetParticlePosition(pos);
   fParticleGun->SetParticleMomentumDirection(mom);
-  fParticleGun->SetParticleMomentum(0.*MeV);
+  fParticleGun->SetParticleMomentum(6.*MeV);
   fParticleGun->SetParticleDefinition(particle);
 
 }
-PrimaryGenerator::~PrimaryGenerator(){
+
+PrimaryGenerator::~PrimaryGenerator()
+{
   delete fParticleGun;
 }
 
-void PrimaryGenerator::GeneratePrimaries(G4Event *anEvent){
+void PrimaryGenerator::GeneratePrimaries(G4Event *anEvent)
+{
   G4ParticleDefinition *particle = fParticleGun->GetParticleDefinition();
 
-  if(particle == G4Geantino::Geantino()){
-    G4int Z = 2;
-    G4int A = 4;
+  if(particle == G4Geantino::Geantino())
+  {
+    G4int Z = 95; //Americio 241
+    G4int A = 241;
 
-    G4double charge = 2.*eplus;
+    G4double charge = 0. *eplus;
     G4double energy = 0.*MeV;
 
     G4ParticleDefinition *ion = G4IonTable::GetIonTable()->GetIon(Z, A, energy);
