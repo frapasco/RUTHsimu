e2535c046b95dcc0ae19521f731f53fcc4b8863a
frapasco <pascolinifrancesco@gmail.com>
2023-02-17 15:39:42 +0000

    added sorting between detectors in analysis.cxx

--- analysis.cxx
+++ analysis.cxx
@@ -18,34 +18,56 @@
 #include <TCanvas.h>
 #include <TText.h>
 
-const Int_t nMaxTotalHits = 500;
-void analysis(){
+const Int_t nMaxTotalHits = 10000;
+
 Int_t EventNo[nMaxTotalHits]={0};
-Double_t XPreStepPoint[nMaxTotalHits]={0.};
-Double_t YPreStepPoint[nMaxTotalHits]={0.};
-Double_t ZPreStepPoint[nMaxTotalHits]={0.};
-Double_t XPostStepPoint[nMaxTotalHits]={0.};
-Double_t YPostStepPoint[nMaxTotalHits]={0.};
-Double_t ZPostStepPoint[nMaxTotalHits]={0.};
+Double_t XPRE[nMaxTotalHits]={0.};
+Double_t YPRE[nMaxTotalHits]={0.};
+Double_t ZPRE[nMaxTotalHits]={0.};
+Double_t XPOST[nMaxTotalHits]={0.};
+Double_t YPOST[nMaxTotalHits]={0.};
+Double_t ZPOST[nMaxTotalHits]={0.};
 Double_t eDep[nMaxTotalHits]={0.};
 Int_t copyNo[nMaxTotalHits]={0};
 
-TString inputFileName="output.root";
-TFile *inFile=new TFile(inputFileName,"READ");
+void readTTree(){  
+  TString inputFileName="output.root";
+  TFile *inFile=new TFile(inputFileName,"READ");
   
-TTree *hitTree=(TTree*)inFile->Get("hitTree");
+  TTree *hitTree=(TTree*)inFile->Get("Hits");
+  
+  // Set branch addresses.
+  hitTree->SetBranchAddress("EventNo",&EventNo);
+  hitTree->SetBranchAddress("XPRE",XPRE);
+  hitTree->SetBranchAddress("YPRE",YPRE);
+  hitTree->SetBranchAddress("ZPRE",ZPRE);
+  hitTree->SetBranchAddress("XPOST",XPOST);
+  hitTree->SetBranchAddress("YPOST",YPOST);
+  hitTree->SetBranchAddress("ZPOST",ZPOST);
+  hitTree->SetBranchAddress("eDep",eDep);
+  hitTree->SetBranchAddress("copyNo",&copyNo);
+  
+  Long64_t nentries = hitTree->GetEntries();
+  std::cout<<"got "<<nentries<<" events"<<std::endl;
 
-// Set branch addresses.
-hitTree->SetBranchAddress("EventNo",&EventNo);
-hitTree->SetBranchAddress("XPreStepPoint",XPreStepPoint);
-hitTree->SetBranchAddress("YPreStepPoint",YPreStepPoint);
-hitTree->SetBranchAddress("ZPreStepPoint",ZPreStepPoint);
-hitTree->SetBranchAddress("XPostStepPoint",XPostStepPoint);
-hitTree->SetBranchAddress("YPostStepPoint",YPostStepPoint);
-hitTree->SetBranchAddress("ZPostStepPoint",ZPostStepPoint);
-hitTree->SetBranchAddress("eDep",eDep);
-hitTree->SetBranchAddress("copyNo",&copyNo);
-    
-Long64_t nentries = hitTree->GetEntries();
-std::cout<<"got "<<nentries<<" events"<<std::endl;
+  inFile->Close();
+}
+
+void analysis(){
+  readTTree();
+  //before target
+  TVector3 PreDetectorPrePos;
+  TVector3 PreDetectorPostPos;
+  //after target
+  TVector3 PostDetectorPrePos;
+  TVector3 PostDetectorPostPos;
+
+  //sorting the hits between pre and post detector
+  if(copyNo==0){ //copyNo = 0 corresponds to the detector before target
+    PreDetectorPrePos=(XPRE,YPRE,ZPRE);
+    PreDetectorPostPos=(XPOST,YPOST,ZPOST);
+  }else{
+    PostDetectorPrePos=(XPRE,YPRE,ZPRE);
+    PostDetectorPostPos=(XPOST,YPOST,ZPOST);
+  }
 }
